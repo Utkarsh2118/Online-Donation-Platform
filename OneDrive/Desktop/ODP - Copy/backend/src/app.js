f1dc78ff4893b3f1ce59b5610e436849572bdc6a
Utkarsh2118 <srivastava.utkarsh2118@gmail.com>
2026-05-01 11:11:32 +0530

    update

--- OneDrive/Desktop/ODP - Copy/backend/src/app.js
+++ OneDrive/Desktop/ODP - Copy/backend/src/app.js
@@ -27,8 +27,8 @@ app.use(
 	})
 );
 app.use(helmet());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 app.use(cookieParser());
 app.use(morgan('dev'));
 
